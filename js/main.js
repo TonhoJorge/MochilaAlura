c173694f4b666e81ece84f8319731ad3c0e0bdd8
tonhojorge <ajbfo@hotmail.com>
2022-08-31 00:19:53 -0300

    Validação de iten negativo com estilização

--- js/main.js
+++ js/main.js
@@ -27,10 +27,17 @@ form.addEventListener("submit", (evento) => {
         itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
         itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
-
-        criaElemento(itemAtual)
-
-        itens.push(itemAtual)
+        
+        if (itemAtual.quantidade > 0 || itemAtual.quantidade === 0) {
+            criaElemento(itemAtual)
+        
+            itens.push(itemAtual)
+        } else {
+            
+            Alert.render('Número de itens inválido')
+            console.log("Algo de errado não está certo")
+                        
+        }
     }
 
     localStorage.setItem("itens",JSON.stringify(itens))
@@ -78,3 +85,25 @@ function deletaElemento (tag, id) {
 
     localStorage.setItem("itens",JSON.stringify(itens))
 }
+
+function CustomAlert(){
+    this.render = function(dialog){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "Erro";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    }
+    this.ok = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
+var Alert = new CustomAlert();
